c748087a9feb5435bf71481b350381d0bd55a0da
Mickey Barboi <damouse007@gmail.com>
2016-05-26 15:53:55 -0500

    added podfile and dson class file

---
+++ DSON/Class.swift
@@ -0,0 +1,11 @@
+//
+//  Class.swift
+//  DSON
+//
+//  Created by Mickey Barboi on 5/26/16.
+//  Copyright © 2016 I. All rights reserved.
+//
+//  This is the components of Silvery classes that didn't really fit into Silvery any longer. 
+//  I'm not entirely happy with this decision, but the code here should be pretty portable
+
+import Foundation
---
+++ Podfile
@@ -0,0 +1,13 @@
+
+workspace 'DSON'
+
+platform :ios, '8.0'
+use_frameworks!
+
+target 'DSON' do
+    pod 'Silvery', :path => '../silvery'
+end
+
+target 'DSONTests' do
+    pod 'Silvery', :path => '../silvery'
+end
